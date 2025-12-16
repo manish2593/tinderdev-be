1e8f2ffff4a423639e01c132ee8c8b295781c314
Manish <manishparmar2593@gmail.com>
2025-12-16 21:34:13 +0530

    Exploring routes

--- src/app.js
+++ src/app.js
@@ -3,10 +3,23 @@ const app = express();
 const { routes } = require('./paths');
 
 for(let i in routes) {
-    app.use(i, (req,res)=> {
+    app.get(i, (req,res)=> {
         res.send(routes[i]);
-    });    
+    });
 }
+
+app.post('/users', (req,res) => {
+    console.log(req .body);
+    res.send("User data has been saved successfully.")
+});
+
+app.put('/user/:id', (req,res) => {
+    res.send("User data has been updated successfully.")
+});
+
+app.delete('/user/:id', (req,res) => {
+    res.send("User has been deleted successfully.")
+});
 
 app.listen(7777, () => {
     console.log("Listening at 7777");
